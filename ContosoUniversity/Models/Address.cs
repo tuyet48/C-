14e40467fc3f89f601bb7c85342f3781cfcd7b4b
LAPTOP-4NERLA1V\Tuyet <sofia.tuyet@gmail.com>
2018-03-03 23:33:54 -0800

    MTCode

---
+++ ContosoUniversity/Models/Address.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+
+namespace ContosoUniversity.Models
+{
+    public class Address
+    {
+        [EmailAddress]
+        [RegularExpression(@"^[a-zA-Z0-9._]+@contoso\.edu$", ErrorMessage = "Invalid Emaddresses. Please entere into the system conform with the pattern [username]@contoso.edu")]
+        public string Email { get; set; }
+    }
+}
---
+++ ContosoUniversity/Models/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContosoUniversity.Models
+{
+    public class Student : Person
+    {
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Enrollment Date")]
+        public DateTime EnrollmentDate { get; set; }
+
+        public int CreditsEarned { get; set; }
+
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+       
+        [Range(0.7, 4.0, ErrorMessage = "Enter a value in range from 0.7 and 4.0")]
+        [DisplayFormat(DataFormatString = "{0:n2}", ApplyFormatInEditMode = true)]
+        public double Gpa { get; set; }      
+    }
+}
